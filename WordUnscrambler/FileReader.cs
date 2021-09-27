9a12e93d12103c8d2c150fcaff5f28296aaa55e1
BrandonLevis <brandonlevis84@gmail.com>
2021-09-27 15:28:01 -0400

    File Reader, Creates an Array to then store what is stored in the text file, it will then read the file from the path's location -shows if string is empty or null to continue

--- WordUnscrambler/FileReader.cs
+++ WordUnscrambler/FileReader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace WordUnscrambler
@@ -11,10 +12,29 @@ namespace WordUnscrambler
     {
         public string[] Read(string filename)
         {
-            // Implement this.
+           
+            string[] Splitter(string input)
+            {
+                return Regex.Split(input, @"\W+");
+            }
+            //Creates Arraylist to store value from File.txt into an Array.
+            List<String[]> arrayList = new List<String[]>();
+
+            //fStream (attribute of name FileStream) will open and read file store in the following path position.
+            using (FileStream fStream = File.OpenRead(@" C: \Users\IT\Documents\.net programming\Lab2\wordlist.txt"))
+            using (TextReader reader = new StreamReader(fStream))
+            {
+                //empty string called line
+                string line = "";
+                //Shows if string is empty or null
+                while (!String.IsNullOrEmpty(line = reader.ReadLine()))
+                {
+                    arrayList.Add(Splitter(line));
+                }
+            }
 
             return null;
-
         }
     }
 }
+
