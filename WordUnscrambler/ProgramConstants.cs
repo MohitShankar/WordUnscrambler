03a87d5cf0c20cb1989b675d868d6c1dda358916
BrandonLevis <brandonlevis84@gmail.com>
2021-09-27 15:30:49 -0400

    While promt every user promt to another class to store them for easy management (change 6)

---
+++ WordUnscrambler/ProgramConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordUnscrambler
+{
+    //Change 6
+    class ProgramConstants
+    {
+        //example
+        public const string PROMPT1 = "Enter a number to add, q to quit";
+
+    }
+}
